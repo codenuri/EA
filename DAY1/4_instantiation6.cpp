d7ec61fe3aeaefcf1b869308905b423a723cc6b2
ecourse <smkang@codenuri.co.kr>
2024-10-21 14:48:54 +0900

    Add files via upload

--- DAY1/4_instantiation6.cpp
+++ DAY1/4_instantiation6.cpp
@@ -8,11 +8,26 @@ class List
 public:
 	List() {}
 	List(int sz, const T& value) {}
+
+	// STL 컨테이너 한개를 인자로 가지는 생성자
+	template<typename C>
+	List(C& c) {}
+
+	template<typename IT>
+	List(IT first, IT last) {}
 };
 
 // 아래 문법은 C++17 부터 가능.
 // 생성자모양 -> List<타입>
 List() -> List<int>;
+
+template<typename C>
+List(C& c) -> List< typename C::value_type >;
+
+template<typename IT>
+//List(IT first, IT last) -> List<반복자IT 가 가리키키는 요소의 타입>
+List(IT first, IT last) -> List< typename std::iterator_traits<IT>::value_type >;
+
 
 int main()
 {
@@ -22,9 +37,9 @@ int main()
 	List      s4; 		 // 디폴트 생성자 자체로는 T 의 추론이 안됨. error
 						 // 되도록 하려면 "class template type deduction guide" 제공. 
 
-//	std::vector v = { 1,2,3 };
-//	List s5(v); // ?
+	std::vector v = { 1,2,3 };
+	List s5(v); // 의미를 생각해 보고, 되도록 해보세요
 
-//	List s6(v.begin(), v.end());
+	List s6(v.begin(), v.end());
 }
 
