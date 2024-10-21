f80820b2800eb7260d0306e9c786614445a89e39
ecourse <smkang@codenuri.co.kr>
2024-10-21 14:31:51 +0900

    Add files via upload

---
+++ DAY1/5_lazy_instantiation4.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+
+struct static_member
+{
+	static_member() { std::cout << "static_member()" << std::endl; }
+	~static_member() { std::cout << "~static_member()" << std::endl; }
+};
+struct instance_member
+{
+	instance_member() { std::cout << "instance_member()" << std::endl; }
+	~instance_member() { std::cout << "~instance_member()" << std::endl; }
+};
+
+struct Object
+{
+	instance_member m1;
+	static static_member m2;
+};
+static_member Object::m2;
+
+int main()
+{
+	std::cout << "--------" << std::endl;
+
+//	Object obj1;
+//	Object obj2;
+
+	std::cout << "--------" << std::endl;
+}
+
+
