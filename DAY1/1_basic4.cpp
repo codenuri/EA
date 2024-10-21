9efd7058b208b3748993cb5b829708f97ac53c0a
ecourse <smkang@codenuri.co.kr>
2024-10-21 13:35:09 +0900

    Add files via upload

---
+++ DAY1/1_basic4.cpp
@@ -0,0 +1,35 @@
+#include <type_traits>
+
+// # 핵심 : template 을 만드는 방법
+
+// #1. typename 또는 class 키워드
+// => C++98 부터 가능.
+//template<typename T>
+template<class T>		// 위와 완전히 동일
+T square(T a)
+{
+	return a * a;
+}
+
+
+// #2. auto : C++20 ~ 
+// => 아래 코드는 위 코드와 완벽히 동일합니다.
+auto square(auto a)
+{
+	return a * a;
+}
+
+
+// #3. requires clauses : C++20 ~
+// => 템플릿 인자에 제약도 전달 가능
+template<typename T> requires std::integeral<T>		
+T square(T a)
+{
+	return a * a;
+}
+
+
+int main()
+{
+
+}
